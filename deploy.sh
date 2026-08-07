234d12ef319f801e0810f78ef0304058ca53a9dd
Md Nuruddin <info.metasoftbd@gmail.com>
2026-08-07 10:35:59 +0000

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+set -e
+
 export HOME=/home/u162248930
 export COMPOSER_HOME=/home/u162248930/.composer
 
-PROJECT="/home/u162248930/domains/metasoftbd.com/apps/shopsaas-git"
+GIT_PROJECT="/home/u162248930/domains/metasoftbd.com/apps/shopsaas-git"
+LIVE_PROJECT="/home/u162248930/domains/metasoftbd.com/apps/shopsaas"
 
 echo "===== MetaSoft SaaS Deployment Started ====="
 
-cd "$PROJECT" || exit 1
+cd "$GIT_PROJECT"
 
 echo "Pulling latest code..."
 git pull origin main
@@ -15,7 +18,17 @@ git pull origin main
 echo "Installing Composer packages..."
 /usr/local/bin/composer install --no-dev --optimize-autoloader --no-interaction
 
+echo "Syncing files to LIVE..."
+rsync -av --delete \
+  --exclude=".git/" \
+  --exclude=".env" \
+  --exclude="storage/" \
+  "$GIT_PROJECT/" \
+  "$LIVE_PROJECT/"
+
 echo "Optimizing Laravel..."
+cd "$LIVE_PROJECT"
+
 php artisan optimize:clear
 php artisan optimize
 
